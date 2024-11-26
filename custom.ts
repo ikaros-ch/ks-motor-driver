409c3c2e5a69796a410273b0946a926847f40027
ikaros-ch <154591960+ikaros-ch@users.noreply.github.com>
2024-11-26 16:12:23 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -15,9 +15,12 @@ enum Direction {
 
 //% color=#0fbc11 icon="\uf085" block="Motor Control"
 namespace MotorControl {
- 
+    //% block="set motor on pin14 to %state"
+    //% state.shadow="toggleOnOff"
     export function setMotorState(state: boolean): void {
         pins.digitalWritePin(DigitalPin.P14, state ? 1 : 0);
+    //% block="set %motor|direction %direction|speed %speed"
+    //% speed.min=0 speed.max=100
     }
     export function controlMotor(motor: Motor, direction: Direction, speed: number): void {
         let pwmPin: AnalogPin;
